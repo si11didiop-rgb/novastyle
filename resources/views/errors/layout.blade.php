e8c18938f916d41becaa502fa2362b28042e7b75
Sidy Diop <si11didiop@gmail.com>
2026-07-27 11:44:50 +0200

    fix: use config app.url in error pages

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -77,9 +77,9 @@
     </head>
     <body>
         <div class="container">
-            <a href="{{ url('/') }}" class="logo">NOVA<span>STYLE</span></a>
+            <a href="{{ config('app.url') }}" class="logo">NOVA<span>STYLE</span></a>
             @yield('content')
-            <a href="{{ url('/') }}" class="btn">Retour à l'accueil</a>
+            <a href="{{ config('app.url') }}" class="btn">Retour à l'accueil</a>
         </div>
     </body>
 </html>
